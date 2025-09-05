c64386763c50e448cedbd9a1746eb5d1ef265f7f
Sam Mulchandani <sammulchandani@Sams-MacBook-Air.local>
2025-09-05 09:43:18 -0400

    add searching algorithms

---
+++ BinarySearch.java
@@ -0,0 +1,49 @@
+//Generate random data
+//Generate random key
+
+// 1) Initialize start variable at index 0,
+// 1) Initialize end variable at index of length - 1
+
+// 2) While start < end:
+    // 3) Intialize mid variable as (start + end) divided by two
+    // 4) Check if element at index of mid variable is key
+    //  4a) Return mid 
+    // 5) Else:
+    //  5a) If key is greater than element at index of mid:
+    //      5a) start variable = mid + 1
+    //  5b) If key is less than element at index of mid:
+    //      5b) end variable = mid
+// 6) If start > end, key not found and:
+//  6a) Return -1
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int binarySearch(int[] list, int key){
+        int start = 0;
+        int end = list.length - 1;
+
+        while(start < end){
+            int mid = (start + end) / 2;
+            if(list[mid] == key) return mid;
+            else if(key > list[mid])
+                start = mid + 1;
+            else if (key < list[mid])
+                end = mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = new int[30];
+        for(int i = 0; i < list.length; i++){
+            list[i] = (int) (Math.random() * 101);
+        }
+        Arrays.sort(list);
+        int key = (int) (Math.random() * 101);
+
+        System.out.println(Arrays.toString(list));
+        System.out.println("Key: " + key);
+        System.out.println(binarySearch(list, key));
+    }
+}
--- LinearSearch.java
+++ LinearSearch.java
@@ -8,10 +8,27 @@
 // 2) Check if element at the current index matches key
 //   2a) Return current index since key found
 // 3) If element at current index does not match key, then increment current index to next element
-// 4) 
-
+// 4) Repeat from step 2 while index is less than length of list
+// 5) If key not found, return -1
+import java.util.Arrays;
 public class LinearSearch{
-    public static void main(String[] args){
+    public static int linearSearch(int[] array, int key){
+        for(int i = 0; i < array.length; i++){
+            if(array[i]==key)
+                return i;
+        }
+        return -1;
+    }
 
+    public static void main(String[] args) {
+        int[] list = new int[30];
+        for(int i = 0; i < list.length; i++){
+            list[i] = (int) (Math.random() * 101);
+        }
+        int key = (int) (Math.random() * 101);
+
+        System.out.println(Arrays.toString(list));
+        System.out.println("Key: " + key);
+        System.out.println(linearSearch(list, key));
     }
 }
