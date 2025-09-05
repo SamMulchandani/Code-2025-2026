c86593bf1274dfb7873d2461be7a116878f4d38c
Sam Mulchandani <sammulchandani@mac.lan>
2025-09-04 21:54:45 -0400

    second commit

---
+++ LinearSearch.java
@@ -0,0 +1,17 @@
+//Generate some random data
+
+//Prompt user for search key
+
+//Linear search algorithm
+
+// 1) Initialize a current index variable to 0
+// 2) Check if element at the current index matches key
+//   2a) Return current index since key found
+// 3) If element at current index does not match key, then increment current index to next element
+// 4) 
+
+public class LinearSearch{
+    public static void main(String[] args){
+
+    }
+}
